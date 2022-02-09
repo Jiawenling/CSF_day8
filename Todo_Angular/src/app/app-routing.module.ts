b55401964540480051c52357dd10192ba3bafa21
Jiawenling <jling002@e.ntu.edu.sg>
2022-02-09 23:31:35 +0800

    working on the version without sidebar

--- Todo_Angular/src/app/app-routing.module.ts
+++ Todo_Angular/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {ShowTaskComponent} from "./show-task/show-task.component";
 
-const routes: Routes = [{path:'',component: MainComponent},
-  {path:'task/:taskId', component:ShowTaskComponent}];
+const routes: Routes = [{path:'',component: MainComponent, children:[{path:'task/:id',component:ShowTaskComponent}]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
